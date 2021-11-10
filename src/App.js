9577c317c5b90e44b972084dfccf75317bd513d4
Hoang Nam <hoangnamqt.2001@gmail.com>
2021-11-10 21:56:37 +0700

    chore(fix variable)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Form from './components/Form'
 import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: 'white',
+      }}
+    >
       <Header />
       <Form />
       <Course />
